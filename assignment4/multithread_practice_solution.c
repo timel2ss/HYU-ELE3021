c3d1d4e0a8e45ace816c61f7c72b14646deba04b
2019054957 <dyddn0823@hanyang.ac.kr>
2021-04-06 22:36:18 +0900

    Assignment 4

--- assignment4/multithread_practice_solution.c
+++ assignment4/multithread_practice_solution.c
@@ -4,8 +4,10 @@
 
 #define ARGUMENT_NUMBER 20
 
-long long total_result = 0;
+// store computational result for each thread
 long long result[ARGUMENT_NUMBER];
+// sum all results for each thread
+long long total_result = 0;
 
 void *ThreadFunc(void *n) {
     long long i;
@@ -13,6 +15,7 @@ void *ThreadFunc(void *n) {
     
     printf("number = %lld\n", number);
     
+    // store 25,000,000 times of argument number to the result of thread
     for(i = 0; i < 25000000; i++) {
         result[number] += number;
     }
@@ -27,17 +30,21 @@ int main(void) {
     for(i = 0; i < ARGUMENT_NUMBER; i++) {
         argument[i] = i;
     }
-
+    
+    // create 20 threads and run ThreadFunc function
     for(i = 0; i < ARGUMENT_NUMBER; i++) {
         pthread_create(&(threadID[i]), NULL, ThreadFunc, (void*)&argument[i]);
     }
    
+    // When all threads are created, print a string on console
     printf("Main Thread is waiting for Child Thread!\n");
 
+    // wait for the end of 20 threads
     for(i = 0; i < ARGUMENT_NUMBER; i++) {
         pthread_join(threadID[i], NULL);
     }
 
+    // add the result of each thread to total_result
     for(i = 0; i < ARGUMENT_NUMBER; i++) {
         total_result += result[i];
     }
