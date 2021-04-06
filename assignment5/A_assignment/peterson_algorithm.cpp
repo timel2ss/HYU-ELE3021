7ccafe9608b5b4ad0a5d356248c54c8766819601
2019054957 <dyddn0823@hanyang.ac.kr>
2021-04-06 22:36:56 +0900

    Assignment 5: A

---
+++ assignment5/A_assignment/peterson_algorithm.cpp
@@ -0,0 +1,63 @@
+#include <atomic>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define COUNTING_NUMBER 2000000
+
+using namespace std;
+
+int critical_section_variable = 0;
+
+atomic_int turn(0);
+atomic_int flag0(0), flag1(0);
+
+void lock(int self) {
+    if(self) {
+        flag1 = 1;
+        turn = 0;
+        while(flag0 == 1 && turn == 0);
+    }
+    else {
+        flag0 = 1;
+        turn = 1;
+        while(flag1 == 1 && turn == 1);
+    }
+}
+
+void unlock(int self) {
+    if(self) {
+        flag1 = 0;
+    }
+    else {
+        flag0 = 0;
+    }
+}
+
+void* func(void* s) {
+    int* thread_num = (int*)s;
+    int i;
+
+    for(i = 0; i < COUNTING_NUMBER; i++) {
+        lock(*thread_num);
+        critical_section_variable++;
+        unlock(*thread_num);
+    }
+}
+
+int main(void) {
+    pthread_t p1, p2;
+
+    int parameter[2] = {0, 1};
+
+    pthread_create(&p1, NULL, func, (void*)&parameter[0]);
+    pthread_create(&p2, NULL, func, (void*)&parameter[1]);
+
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+
+    printf("Actual Count: %d | Expected Count: %d\n", critical_section_variable, COUNTING_NUMBER * 2);
+
+    return 0;
+}
+
